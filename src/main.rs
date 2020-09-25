b950095fff07b35835547aaa524bb237a29fc6e5
Lukasz Konopka <lukeKonopka@protonmail.com>
2020-09-25 23:09:01 +0200

    Use rust-pulsectl to connect with pulseaudio

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,7 @@ impl TryFrom<&[u8; 16]> for NommoMsg {
             [1, 233, ..] => Ok(Self::VolUp),
             [1, 234, ..] => Ok(Self::VolDown),
             [5, 15, _, v, ..] => Ok(Self::EqValue(*v)),
-            [1, 0, ..] => Ok(Self::Noop),
-            _ => Err(format!("Unknown msg: {:?}", other)),
+            _ =>  Ok(Self::Noop),
         }
     }
 }
@@ -88,96 +87,53 @@ impl Display for NommoVol {
     }
 }
 
-fn get_default_sink_name() -> subprocess::Result<String> {
-    let pactl_info_default_sink = {
-        Exec::shell("pactl info")
-            | Exec::cmd("grep").arg("Default Sink")
-            | Exec::cmd("tail").arg("--bytes=+15")
-    }
-    .capture()?
-    .stdout_str();
+use pulsectl::controllers::DeviceControl;
+use pulsectl::controllers::SinkController;
 
-    let name: String = pactl_info_default_sink.split_whitespace().collect();
+fn handle_device(dev_handle: hidapi::HidDevice) {
+    // get Pulse Audio default device
+    let mut sink_controller = SinkController::create();
+    let default_sink = sink_controller.get_default_device().expect("Cannot get PulseAudio default sink");
 
-    match name.len() {
-        0 => Err(subprocess::PopenError::LogicError(
-            "No default sink detected",
-        )),
-        _ => Ok(name),
-    }
-}
-
-fn get_sink_volume(sink_name: &str) -> subprocess::Result<NommoVol> {
-    let pactl_sink_volume = {
-        Exec::shell("pactl list sinks")
-            | Exec::cmd("grep")
-                .arg("-A 10")
-                .arg(format!("Name: {}", sink_name))
-            | Exec::cmd("grep").arg("-m 1").arg("Volume")
-            | Exec::cmd("cut").arg("--delimiter=/").arg("--fields=2")
-    }
-    .capture()?
-    .stdout_str();
-
-    let volume = pactl_sink_volume.split_whitespace().collect::<String>();
-    let nommo_volume = NommoVol::try_from(volume).unwrap_or(NommoVol::Muted);
-    Ok(nommo_volume)
-}
-
-fn set_sink_mute(sink_name: &str, mute: bool) -> subprocess::Result<()> {
-    let mute_val = if mute { 1 } else { 0 };
-    Exec::shell(format!("pactl set-sink-mute {} {}", sink_name, mute_val))
-        .join()
-        .map(|_| ())?;
-
-    if mute {
-        println!("[nommo] Set mute to true");
-    }
-
-    Ok(())
-}
-
-fn set_sink_volume(sink_name: &str, vol: NommoVol) -> subprocess::Result<()> {
-    set_sink_mute(sink_name, vol == NommoVol::Muted)?;
-
-    Exec::shell(format!("pactl set-sink-volume {} {}", sink_name, vol))
-        .join()
-        .map(|_| ())?;
-
-    println!("[nommo] Set volume to: {}", vol);
-
-    Ok(())
-}
-
-fn handle_device(dev_handle: hidapi::HidDevice, sink_name: &str) {
     let mut buff = [0 as u8; 16];
     loop {
         dev_handle.read(&mut buff).expect("Cannot read from device");
         let msg = NommoMsg::try_from(&buff).expect("Cannot convert data");
-        let current_vol = get_sink_volume(&sink_name).expect("Cannot get volume");
 
         match msg {
             NommoMsg::VolUp => {
-                set_sink_volume(&sink_name, current_vol.inc(VOL_STEP))
-                    .expect("Cannot increase volume");
+                // TODO: add support for unmuting
+                // TODO: add 100% cap
+                sink_controller.increase_device_volume_by_percent(default_sink.index, VOL_STEP as f64 / 100.0);
             }
             NommoMsg::VolDown => {
-                set_sink_volume(&sink_name, current_vol.dec(VOL_STEP))
-                    .expect("Cannot decrease volume");
+                // TODO: add support for muting
+                sink_controller.decrease_device_volume_by_percent(default_sink.index, VOL_STEP as f64 / 100.0);
             }
             _ => {}
         }
     }
 }
 
+fn debug_user_name() -> subprocess::Result<()> {
+    let whoami_res = { Exec::shell("whoami") }.capture()?.stdout_str();
+
+    println!("Current user: {}", whoami_res);
+    Ok(())
+}
+
+
 fn main() {
-    let default_sink_name = get_default_sink_name().expect("Cannot get default sink name");
+    debug_user_name().expect("Cannot debug print username");
+
+    // let default_sink_name = get_default_sink_name().expect("Cannot get default sink name");
+
 
     match hidapi::HidApi::new() {
         Ok(api) => {
             let device = api.open(NOMMO_VENDOR_ID, NOMMO_PRODUCT_ID);
             match device {
-                Ok(handle) => handle_device(handle, &default_sink_name),
+                Ok(handle) => handle_device(handle),
                 Err(error) => {
                     eprintln!("Device error: {}", error);
                 }
